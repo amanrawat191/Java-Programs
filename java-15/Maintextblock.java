6bfbdb85e21364ed9235ec756a37e3390c9c2cd7
Aman Rawat <az.amanrawat191@gmail.com>
2026-02-23 12:56:43 +0530

    new file

---
+++ java-15/Maintextblock.java
@@ -0,0 +1,24 @@
+public class Maintextblock {
+    public static void main(String[] args) {
+        String data="""
+                public class MainVar {
+    public static void main(String[] args) {
+        int a=10;
+        var b=20;//after java 10 we can use var keyword in java with some limitation 
+        var name1="kiet mca"; //local variable type inference
+        var value=10.34;
+          //type inference means we have to give it type at declaration 
+        //initialisation is must at the time of declaration like var=10; 
+        String name=new String("This is String");
+        var nam3=new String ("This is string with var");
+        int arr[]={1,2,4,5,6};
+        for(var da: arr){
+          System.out.println(da);
+        }
+}
+}
+
+                """;
+            System.out.println(data);
+    }
+}
